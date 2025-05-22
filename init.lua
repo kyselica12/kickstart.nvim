9131eba9c66b5fa9236e48741e68b95facf94e9c
Daniel Kyselica <danielkyselica@Daniels-MacBook-Pro.local>
2025-05-22 13:05:44 +0200

    tab completion

--- init.lua
+++ init.lua
@@ -836,6 +836,7 @@ require('lazy').setup({
     --- @type blink.cmp.Config
     opts = {
       keymap = {
+        -- ['<Tab>'] = { 'accept' },
         -- 'default' (recommended) for mappings similar to built-in completions
         --   <c-y> to accept ([y]es) the completion.
         --    This will auto-import if your LSP supports it.
@@ -857,7 +858,7 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
+        preset = 'super-tab',
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
